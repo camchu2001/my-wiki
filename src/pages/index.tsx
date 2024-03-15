967c6e14f63bc044aa4136a00fb3c6c711574c05
camchu2001 <chuphuongcam@gmail.com>
2024-03-15 12:34:01 -0500

    chore: remove hompage header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,20 +6,6 @@ import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
-
 export default function Home(): JSX.Element {
   return (
     <Layout
